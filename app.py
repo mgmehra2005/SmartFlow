a1d8a944c21bb8de439380b72b4354e9577a75ac
Matang Mehra <matangmehra+public@gmail.com>
2026-02-10 21:46:17 +0530

    UI Update V.2.0

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from smartflow_engine import Member, Task, assignTasks
 
 app = Flask(__name__)
 
-members = []
-tasks = []
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -17,26 +15,10 @@ def assign():
     # print("Received Members: ", members)
     # print("Received Tasks: ", tasks)
     assignments = assignTasks(members, tasks)
-    jsonedData = jsonify({"assignments": assignments})
-    # print("Assignments from Smartflow Engine: ", jsonedData.get_json())
-    return jsonedData
 
+    print("Assignments from Smartflow Engine: ", assignments)
+    return jsonify({"assignments": assignments})
 
-# @app.route("/members", methods=["POST"])
-# def assignMembers():
-#     data = request.get_json()
-#     for member in data["members"]:
-#         members.append(Member(member["id"], member["name"], member["skills"], member["availability"]))
-#         print("Added Member: ", member["name"], member["skills"], member["availability"])
-#     return jsonify({"status":200})
-
-# @app.route("/tasks", methods=["POST"])
-# def addTasks():
-#     data = request.get_json()
-#     for task in data["tasksList"]:
-#         tasks.append(Task(task["id"], task["title"], task["required_skills"], task["priority"], task["effort"]))
-#         print("Added Task: ", task["title"], task["required_skills"], task["priority"], task["effort"])
-#     return jsonify({"status":200})
 
 @app.route("/createTeam")
 def createTeam():
@@ -46,14 +28,7 @@ def createTeam():
 def addTeamMember():
     return render_template("addTeamMember.html")
 
-<<<<<<< HEAD
-@app.route("/createTasks")
-def createTasks():
-    return render_template("createTasks.html")
 
-@app.route("/assignedTasks")
-def assignedTasks():
-    return render_template("assignedTask.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
